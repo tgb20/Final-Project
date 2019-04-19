e22363da00235e67a2ec240bfa8b160a52e1f9bc
Jacob Bashista <jacobbashista@1x-nat-vl931-172-30-187-8.wireless.umass.edu>
2019-04-19 15:16:54 -0400

    Removed Min and Max Temp

--- Python Scripts/dataCleaner.py
+++ Python Scripts/dataCleaner.py
@@ -1,7 +1,7 @@
 import json
 import csv
 with open('../Data/cleaned_data.csv', 'w') as csvfile:
-    fieldnames = ['id', 'weathertype', 'temp', 'pressure', 'humidity', 'mintemp', 'maxtemp', 'windspeed', 'winddeg', 'cloudcov', 'dt', 'dtiso']
+    fieldnames = ['id', 'weathertype', 'temp', 'pressure', 'humidity', 'windspeed', 'winddeg', 'cloudcov', 'dt', 'dtiso']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
@@ -29,15 +29,13 @@ with open('../Data/cleaned_data.csv', 'w') as csvfile:
             temp = p["main"]["temp"]
             pressure = p["main"]["pressure"]
             humidity = p["main"]["humidity"]
-            mintemp = p["main"]["temp_min"]
-            maxtemp = p["main"]["temp_max"]
             windspeed = p["wind"]["speed"]
             winddeg = p["wind"]["deg"]
             cloudcov = p["clouds"]["all"]
             dt = p["dt"]
             dtiso = p["dt_iso"]
 
-            writer.writerow({'id': pID, 'weathertype': weathertype, 'temp': temp, 'pressure': pressure, 'humidity': humidity, 'mintemp': mintemp, 'maxtemp': maxtemp, 'windspeed': windspeed,'winddeg': winddeg, 'cloudcov': cloudcov, 'dt': dt, 'dtiso': dtiso})
+            writer.writerow({'id': pID, 'weathertype': weathertype, 'temp': temp, 'pressure': pressure, 'humidity': humidity, 'windspeed': windspeed,'winddeg': winddeg, 'cloudcov': cloudcov, 'dt': dt, 'dtiso': dtiso})
             pID += 1
             
         
